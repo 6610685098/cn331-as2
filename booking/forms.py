13f34e1ea0c8a083737adc25910e9c836e6b55b5
Kittidet Wichaidit <6610685098@student.tu.ac.th>
2025-09-14 17:13:47 +0700

    Add comment

--- booking/forms.py
+++ booking/forms.py
@@ -1,11 +1,9 @@
-# booking/forms.py
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from .models import Room
 
-# --- ฟอร์มสำหรับสมัครสมาชิก (ฉบับแก้ไข) ---
+# ฟอร์มสมัครสมาชิก (User Registration)
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
@@ -13,8 +11,6 @@ class CustomUserCreationForm(UserCreationForm):
         self.fields['username'].widget.attrs.update({'placeholder': 'Username...'})
         self.fields['email'].widget.attrs.update({'placeholder': 'Email...'})
         
-        # === จุดที่แก้ไข ===
-        # เปลี่ยนจาก 'password' เป็น 'password1'
         self.fields['password1'].widget.attrs.update({'placeholder': 'Password...'})
         self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm Password...'})
 
@@ -22,8 +18,7 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ('username', 'email')
 
-
-# --- ฟอร์มสำหรับจัดการห้อง (เหมือนเดิม) ---
+# ฟอร์มห้องเรียน (Room)
 class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
